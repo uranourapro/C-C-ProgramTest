c1c61d7b338bcd890bde77b440828a65576da9d2
田中健太 <dorakenn1215@gmail.com>
2019-03-09 11:29:13 +0900

    ファイル読み書き消しの練習です

--- FileLoad/FileLoad/KentaFile.cpp
+++ FileLoad/FileLoad/KentaFile.cpp
@@ -1,5 +1,55 @@
 ﻿#include"KentaFile.h"
+#include<fstream>
+#include<string>
+
+#define FN "test2.txt"
 
 
+bool Test1() {//インプット入力読み込み
+	std::ifstream ifs(FN);
+	std::string str;
 
+	if (ifs.fail()) {
+		std::cout << "ファイルの読み込みに失敗しました" << std::endl;
+		return false;
+	}
+
+	while (std::getline(ifs, str)) {
+		std::cout << str << std::endl;
+	}
+
+	return true;
+}
+
+bool Test2(std::string str) {//アウトプット出力書き込み
+	std::ofstream ofs(FN, std::ios::app);
+
+	ofs << str << "\n";//書き込み
+	
+	ofs.close();//終了
+
+	return true;
+}
+
+void Test3() {//アウトプット
+	std::ofstream ofs(FN, std::ios::app);
+	std::string str;
+
+	if (ofs.fail()) {
+		std::cout << "ファイルが存在しなかったため、作成しました" << std::endl;
+	}
+
+	std::cout << "ファイルに書き込みたい文字を入力してください" << std::endl;
+
+	std::cin >> str;//ユーザー入力
+
+	ofs << str << "\n";//書き込み
+
+	ofs.close();
+}
+
+void TestFileDelete() {
+	std::ofstream ofs(FN, std::ios::trunc);
+	
+}
 
--- FileLoad/FileLoad/main.cpp
+++ FileLoad/FileLoad/main.cpp
@@ -1,9 +1,17 @@
 ﻿#include"KentaFile.h"
-#include<iostream>
 
 int main() {
 
+	while (true) {
+		Test2("書き込み完了");
+		getchar();
 
+		Test1();
+		getchar();
+
+		TestFileDelete();
+		getchar();
+	}
 
 	system("pause");
 	return 0;
